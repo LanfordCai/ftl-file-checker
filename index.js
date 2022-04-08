830c0969d689078a0794be77029b94b432d19b09
lanford <lanfordcai@outlook.com>
2022-04-08 23:42:20 +0800

    init commit

--- index.js
+++ index.js
@@ -1,21 +1,116 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require('@actions/core')
+const github = require('@actions/github')
 
+const REGISTRY_DIR = "token-registry"
+const VALID_FILES = [
+  "logo.png",
+  "token.json",
+  "logo-large.png",
+  "logo.svg",
+  "testnet.token.json"
+]
 
-// most @actions toolkit packages have async methods
+const REQUIRED_FILES = [
+  "logo.png", 
+  "token.json"
+]
+
+run()
+
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const owner = github.context.repo.owner
+    const repo = github.context.repo.repo
+    const prNumber = github.context.payload.pull_request.number
+    const client = getOctokit()
+    const files = pullFiles(client, owner, repo, prNumber)
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const checkNewToken = core.getInput("checkNewToken") || false
+    const checkUpdateToken = core.getInput("checkUpdateToken") || false
 
-    core.setOutput('time', new Date().toTimeString());
+    validateFiles(files)
+
+    if (checkNewToken) {
+      checkNewTokenFiles(files)
+    } else if (checkUpdateToken) {
+      checkUpdateTokenFiles(files)
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run();
+function checkNewTokenFiles(files) {
+  let hasLogo = false
+  let hasTokenJson = false
+  for (var i = 0; i < files.length; i++) {
+    const file = files[i]
+    if (file.status != "added") {
+      throw new Error("only add new file is allowed in a NewToken PR")
+    }
+    const [registryDir, tokenSymbol, filename] = file.filename.split("/") 
+    if (!VALID_FILES.includes(filename)) {
+      throw new Error("contains invalid file")
+    }
+    if (filename == "logo.png") {
+      hasLogo = true
+    }
+    if (filename == "token.json") {
+      hasTokenJson = true
+    }
+  }
+
+  if (!(hasLogo && hasTokenJson)) {
+    throw new Error("logo.png and token.json is required")
+  }
+}
+
+function checkUpdateTokenFiles(files) {
+  for (var i = 0; i < files.length; i++) {
+    const file = files[i]
+    const [registryDir, tokenSymbol, filename] = file.filename.split("/") 
+    if (!VALID_FILES.includes(filename)) {
+      throw new Error("contains invalid file")
+    } 
+    if (file.status == "added" && (filename == "logo.png" || filename == "token.json")) {
+      throw new Error("seems add new token rather than update token")
+    }
+  }
+}
+
+function validateFiles(files) {
+  let symbol = null
+  if (files.length > 5 || files.length == 0) { 
+    throw new Error("invalid files count") 
+  }
+  for (var i = 0; i < files.length; i++) {
+    const file = files[i]
+    const [registryDir, tokenSymbol, filename] = file.filename.split("/")
+    if (registryDir != REGISTRY_DIR) { 
+      throw new Error(`changes happened out of ${REGISTRY_DIR}`) 
+    }
+    if (tokenSymbol.trim() == "") { 
+      throw new Error(`invalid tokenSymbol ${tokenSymbol}`) 
+    }
+    if (!symbol) {
+      symbol = tokenSymbol
+    }
+    if (symbol != tokenSymbol) {
+      throw new Error(`more than one token changed! ${symbol} && ${tokenSymbol}`)
+    }
+  }
+}
+
+async function pullFiles(client, owner, repo, prNumber) {
+  return await client.rest.pulls.listFiles({
+    owner: owner,
+    repo: repo,
+    pull_number: prNumber,
+  })
+}
+
+function getOctokit() {
+  const gh_token = process.env.GITHUB_TOKEN
+  const octokit = github.getOctokit(token=gh_token)
+  return octokit
+}
