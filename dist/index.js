f23b4453ca9992ea43db70747cb29767a0071feb
lanford <lanfordcai@outlook.com>
2022-04-09 16:56:30 +0800

    v0.0.1 release

--- dist/index.js
+++ dist/index.js
@@ -15746,6 +15746,7 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(2186)
 const github = __nccwpck_require__(5438)
+const path = __nccwpck_require__(5622)
 const Ajv = __nccwpck_require__(4941)
 
 const REGISTRY_DIR = "token-registry"
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const path = require('path')
 const Ajv = require("ajv")
 
 const REGISTRY_DIR = "token-registry"
