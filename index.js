7e4084bbfb31bc46e62ec7f435b7371eb44a1d25
lanford33 <lanfordcai@outlook.com>
2022-04-12 01:22:46 +0800

    V0.0.2 (#6)

--- index.js
+++ index.js
@@ -13,141 +13,173 @@ const VALID_FILES = [
   "testnet.token.json"
 ]
 
-let symbol = null
+let client = null
+let owner = null
+let repo = null
+let prNumber = null
+let ref = null
+
+let tokenUUID = null
 
 run()
 
 async function run() {
   try {
-    const owner = github.context.repo.owner
-    const repo = github.context.repo.repo
-    const prNumber = github.context.payload.pull_request.number
-    const client = getOctokit()
-
-    const labelsResp = await getLabels(client, owner, repo, prNumber)
-    if (labelsResp.status != 200) {
-      throw new Error("get labels failed")
-    }
-    const labels = labelsResp.data
-
-    const filesResp = await pullFiles(client, owner, repo, prNumber)
-    if (filesResp.status != 200) {
-      throw new Error("pull files failed")
-    }
-    const files = filesResp.data.filter((file) => { return file.status != "removed"} )
+    owner = github.context.repo.owner
+    repo = github.context.repo.repo
+    prNumber = github.context.payload.pull_request.number
+    client = getOctokit()
+    ref = core.getInput("REF")
+    console.log(ref)
 
     const shouldValidateImages = core.getInput("VALIDATE_IMAGES")
 
-    if (labels.some((label) => { return label.name == "NewToken" })) {
-      core.info(`[NewToken] start checking files`)
-      validateFiles(files)
-      checkNewTokenFiles(files)
-      core.info(`[NewToken] start validating json files`)
-      await validateJsonFiles(client, owner, repo, files, symbol)
-      if (shouldValidateImages) {
-        core.info(`[NewToken] start validating images`)
-        await validateImages(client, owner, repo, files)
-      }
-    } else if (labels.some((label) => { return label.name == "UpdateToken" })) {
-      core.info(`[UpdateToken] start checking files`)
-      validateFiles(files)
-      checkUpdateTokenFiles(files)
-      core.info(`[UpdateToken] start validating json files`)
-      await validateJsonFiles(client, owner, repo, files, symbol)
-      if (shouldValidateImages) {
-        core.info(`[UpdateToken] start validating images`)
-        await validateImages(client, owner, repo, files)
-      }
-    } else {
+    const labels = await getLabels()
+    const withNewTokenLabel = labels.some((label) => label.name == "NewToken")
+    const withUpdateTokenLabel = labels.some((label) => label.name == "UpdateToken")
+
+    if (!withNewTokenLabel && !withUpdateTokenLabel) {
       core.info(`This PR is UnrelatedToToken`)
+      return
     }
 
+    const files = (await getPRFiles())
+      .filter((file) => { return file.status != "removed"} )
+      
+    validateChangedFiles(files)
+    await validateDirectory()
+
+    core.info("start validating json files")
+    await validateJsonFiles(files)
+
+    if (shouldValidateImages) {
+      core.info("start validating images")
+      await validateImages(files)
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-function checkNewTokenFiles(files) {
+function validateChangedFiles(files) {
+  if (files.length > 5 || files.length == 0) { 
+    throw new Error("invalid files count") 
+  }
+
+  let registryDirs = []
+  let tokenUUIDs = []
+  for (var i = 0; i < files.length; i++) {
+    const [registryDir, tokenUUID,] = files[i].filename.split("/")
+    registryDirs.push(registryDir)
+    tokenUUIDs.push(tokenUUID)
+  }
+
+  const registryDirSet = new Set(registryDirs)
+  if (registryDirSet.size > 1) {
+    throw new Error(`modifications are only allowed within ${REGISTRY_DIR}`) 
+  }
+
+  const tokenUUIDSet = new Set(tokenUUIDs) 
+  if (tokenUUIDSet.size > 1) {
+    throw new Error(`more than one token changed!`)
+  }
+  
+  tokenUUID = tokenUUIDs[0]
+}
+
+async function validateDirectory() {
+  const files = await getTokenDirectory(tokenUUID)
   let hasLogo = false
   let hasTokenJson = false
   for (var i = 0; i < files.length; i++) {
     const file = files[i]
-    if (file.status != "added") {
-      throw new Error("only adding new files is allowed in a NewToken PR")
-    }
-    const [,, filename] = file.filename.split("/") 
-    if (!VALID_FILES.includes(filename)) {
-      core.info(`contains invalid file: ${file.filename}`)
+    if (!VALID_FILES.includes(file.name)) {
+      core.info(`contains invalid file: ${file.name}`)
       core.info(`valid files are:`)
       VALID_FILES.forEach((filename) => {
         core.info(`\u001b[38;2;255;0;0m${filename}`) 
       })
       throw new Error("contains invalid file")
     }
-    if (filename == "logo.png") {
+
+    if (file.name == "logo.png") {
       hasLogo = true
     }
-    if (filename == "token.json") {
+    if (file.name == "token.json") {
       hasTokenJson = true
     }
   }
 
   if (!(hasLogo && hasTokenJson)) {
-    throw new Error("logo.png and token.json are required for NewToken PR")
+    throw new Error("logo.png and token.json are required")
   }
 }
 
-function checkUpdateTokenFiles(files) {
+async function validateJsonFiles(files) {
+  const schemaPath = core.getInput("TOKEN_JSON_SCHEMA_PATH") 
+  console.log(`token json schema path: ${schemaPath}`)
+  
+  const schema = JSON.parse(await getFileContent(schemaPath, "raw", "main"))
   for (var i = 0; i < files.length; i++) {
     const file = files[i]
-    const [,, filename] = file.filename.split("/") 
-    if (!VALID_FILES.includes(filename)) {
-      core.info(`contains invalid file: ${file.filename}`)
-      core.info(`valid files are:`)
-      VALID_FILES.forEach((filename) => {
-        core.info(`\u001b[38;2;255;0;0m${filename}`) 
-      })
-      throw new Error("contains invalid file")
-    } 
-    if (file.status == "added" && (filename == "logo.png" || filename == "token.json")) {
-      throw new Error("this seems like a NewToken PR rather than an UpdateToken PR")
+    if (path.extname(file.filename) != ".json") {
+      continue
     }
+  
+    await validateSingleJsonFile(file, schema)
   }
 }
 
-function validateFiles(files) {
-  if (files.length > 5 || files.length == 0) { 
-    throw new Error("invalid files count") 
+async function validateSingleJsonFile(file, schema)  {
+  const json = JSON.parse(await getFileContent(file.filename, "raw", ref))
+
+  // const uuid = `${json.address}.${json.contractName}`
+  const uuid = json.symbol
+  core.info(uuid)
+  core.info(tokenUUID)
+  if (tokenUUID != uuid) {
+    throw new Error("UUIDs in path and token.json are mismatch")
   }
-  for (var i = 0; i < files.length; i++) {
-    const file = files[i]
-    const [registryDir, tokenSymbol,] = file.filename.split("/")
-    if (registryDir != REGISTRY_DIR) { 
-      throw new Error(`modifications are only allowed within ${REGISTRY_DIR}`) 
-    }
-    if (tokenSymbol.trim() == "") { 
-      throw new Error(`invalid token symbol: ${tokenSymbol}`) 
-    }
-    if (!symbol) {
-      symbol = tokenSymbol
-    }
-    if (symbol != tokenSymbol) {
-      throw new Error(`more than one token changed: ${symbol} && ${tokenSymbol}`)
-    }
+
+  core.info(`validating ${file.filename}`)
+  const validate = ajv.compile(schema)
+  const valid = validate(json)
+  if (!valid) {
+    core.info(`--------------------------------------------------------`)
+    core.info(`\u001b[38;2;255;0;0m${file.filename} is invalid`)
+    validate.errors.forEach((err) => {
+      core.info(`\u001b[38;2;255;0;0m${err.message}`)
+    })
+    core.info(`--------------------------------------------------------`)
+    throw new Error("invalid json file detected")
   }
+  core.info(`${file.filename} is valid`)
+
+  await validateUniqueness(file.filename, json)
 }
 
-async function pullFiles(client, owner, repo, prNumber) {
-  return await client.rest.pulls.listFiles({
-    owner: owner,
-    repo: repo,
-    pull_number: prNumber,
-  })
+async function validateUniqueness(filename, json) {
+  let tokenlistPath = "src/tokens/flow-mainnet.tokenlist.json" 
+  if (filename == "testnet.token.json") {
+    tokenlistPath = "src/tokens/flow-testnet.tokenlist.json" 
+  }
+
+  const tokenlist = JSON.parse(await getFileContent(tokenlistPath, "raw", "main"))
+  const names = tokenlist.tokens.map((token) => token.name)
+  if (names.includes(json.name)) {
+    throw new Error("token name duplicated")
+  }
+
+  const uuids = tokenlist.tokens.map((token) => `${token.address}.${token.contractName}`)
+  if (uuids.includes(`${json.address}.${json.contractName}`)) {
+    throw new Error("{tokenAdress}.{tokenContractName} duplicated")
+  }
+  core.info(`${filename} isn't existed yet`)
 }
 
-async function validateImages(client, owner, repo, files) {
+async function validateImages(files) {
   const imageMaxSize = core.getInput("IMAGE_MAX_SIZE") 
-  console.log(`image max size is ${imageMaxSize}`) 
+  core.info(`image max size is ${imageMaxSize}`) 
 
   for (var i = 0; i < files.length; i++) {
     const file = files[i]
@@ -156,88 +188,79 @@ async function validateImages(client, owner, repo, files) {
       continue
     }
   
-    const resp = await getFileContent(client, owner, repo, file, "json")
-    if (resp.status != 200) {
-      throw new Error(`fetch image failed: ${file.filename}`)
-    }
-    if (resp.data.size > imageMaxSize) {
-      const msg = `The size of ${file.filename} is ${resp.data.size} bytes, exceeding the max size(${imageMaxSize} bytes)`
+    const data = await getFileContent(file.filename, "json", ref)
+    core.info(`validating ${file.filename}`)
+    if (data.size > imageMaxSize) {
+      const msg = `The size of ${file.filename} is ${data.size} bytes, exceeding the max size(${imageMaxSize} bytes)`
       core.info(`\u001b[38;2;255;0;0m${msg}`)
       throw new Error(msg)
     }
+    core.info(`${file.filename} is valid`)
   } 
 }
 
-async function validateJsonFiles(client, owner, repo, files, symbol) {
-  const resp = await getJsonSchema(client, owner, repo)
-  if (resp.status != 200) {
-    throw new Error("fetch json schema failed")
-  }
-  const schema = JSON.parse(resp.data)
-
-  for (var i = 0; i < files.length; i++) {
-    const file = files[i]
-    if (path.extname(file.filename) != ".json") {
-      continue
-    }
-  
-    const resp = await getFileContent(client, owner, repo, file, "raw")
-    if (resp.status != 200) {
-      throw new Error(`fetch json file failed: ${file.filename}`)
-    }
-
-    const data = JSON.parse(resp.data)
-    if (data.symbol != symbol) {
-      throw new Error(`symbols in path and ${file.filename} are mismatched`)
-    }
-
-    const validate = ajv.compile(schema)
-    const valid = validate(data)
-    if (!valid) {
-      core.info(`--------------------------------------------------------`)
-      core.info(`\u001b[38;2;255;0;0m${file.filename} is invalid`)
-      validate.errors.forEach((err) => {
-        core.info(`\u001b[38;2;255;0;0m${err.message}`)
-      })
-      core.info(`--------------------------------------------------------`)
-      throw new Error("invalid json file detected")
-    }
-  }
-}
-
-async function getJsonSchema(client, owner, repo) {
-  const contentPath = core.getInput("TOKEN_JSON_SCHEMA_PATH") 
-  console.log(`token json schema path: ${contentPath}`)
-
-  return await client.rest.repos.getContent({
-    mediaType: {
-      format: ["raw"],
-    },
-    owner: owner, 
-    repo: repo,
-    path: contentPath,
-  })
-}
-
-async function getFileContent(client, owner, repo, file, format) {
-  const [, ref] = file.contents_url.split("ref=")
-  return await client.rest.repos.getContent({
+async function getFileContent(path, format, ref) {
+  const resp = await client.rest.repos.getContent({
     mediaType: {
       format: [format],
     },
     owner: owner, 
     repo: repo,
-    path: file.filename,
+    path: path,
     ref: ref
   })
+
+  if (resp.status != 200) {
+    throw new Error(`get file failed: ${path}`)
+  }
+
+  return resp.data
 }
 
-async function getLabels(client, owner, repo, prNumber) {
-  return await client.rest.issues.listLabelsOnIssue({
+async function getTokenDirectory(tokenUUID) {
+  const resp = await client.rest.repos.getContent({
+    mediaType: {
+      format: ["raw"],
+    },
+    owner: owner, 
+    repo: repo,
+    path: `${REGISTRY_DIR}/${tokenUUID}`,
+    ref: ref
+  })
+
+  if (resp.status != 200) {
+    throw new Error(`get directory contents failed: ${resp.status}`)
+  }
+
+  return resp.data
+}
+
+async function getLabels() {
+  const resp = await client.rest.issues.listLabelsOnIssue({
     owner: owner,
     repo: repo,
     issue_number: prNumber
   })
+
+  if (resp.status != 200) {
+    throw new Error("get labels failed")
+  }
+
+  return resp.data
+}
+
+async function getPRFiles() {
+  const resp = await client.rest.pulls.listFiles({
+    owner: owner,
+    repo: repo,
+    pull_number: prNumber,
+  })
+
+  if (resp.status != 200) {
+    throw new Error("get PR files failed")
+  }
+
+  return resp.data
 }
 
 function getOctokit() {
